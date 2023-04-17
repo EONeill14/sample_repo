f788936baaa6fc665bd5f05faaf557ff5c426b57
emmet <emmet@DESKTOP-RLC5DHD.station>
2023-04-17 21:17:40 +0100

    Added POM and amending main program.

--- sample/src/main/java/com/mycompany/sample/Sample.java
+++ sample/src/main/java/com/mycompany/sample/Sample.java
@@ -8,5 +8,6 @@ public class Sample {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Adding an extrs line for more to be committed");
     }
 }
