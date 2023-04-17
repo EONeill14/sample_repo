7390e7cfee3f7a7d422beecefbf55a793f8e9a24
emmet <emmet@DESKTOP-RLC5DHD.station>
2023-04-17 21:12:20 +0100

    Added a main program and set up the IDE project.

---
+++ sample/src/main/java/com/mycompany/sample/Sample.java
@@ -0,0 +1,12 @@
+package com.mycompany.sample;
+
+/**
+ *
+ * @author emmet
+ */
+public class Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
